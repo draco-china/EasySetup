a8b1dbc4b6276d233664fa716b3d0d9c6817d710
Draco <draco.coder@gmail.com>
2021-11-30 13:09:31 +0800

    Update brew_install_app_for_mac.sh

--- brew_install_app_for_mac.sh
+++ brew_install_app_for_mac.sh
@@ -14,46 +14,47 @@ install_homebrew(){
 
 # 添加软件仓库
 brew_tap_list(){
-	brew tap homebrew/cask
-	brew tap homebrew/cask-versions
-	brew tap homebrew/core
-	brew tap homebrew/services
-	brew tap mongodb/brew
+	# brew tap homebrew/cask
+	# brew tap homebrew/cask-versions
+	# brew tap homebrew/core
+	# brew tap homebrew/services
+	# brew tap mongodb/brew
 }
 
 # GUI软件包清单
 # 在这里添加或者删除你需要的GUI软件包名称
 
 brew_cask_app_list=(
-	# adobe-creative-cloud			# Adobe
-	# aliworkbench							# 千牛卖家工作台
-	battle-net									# 暴雪战网
-	# charles										# 抓包神器 / Mock工具
-	cheatsheet									# 应用快捷键提示
-	clashx-pro									# clashx
-	dash          							# Mac专属的文档管理工具
-	google-chrome								# Google 浏览器
-	iina												# 视频播放器
-	iterm2											# 终端
-	# lark											# 飞书
-	# motrix										# 一款全能的下载工具
-	parallels									# 虚拟机
-	# platelet									# 血小板
-	pock												# Touchbar 自定义
-	# postman										# API 请求测试
-	qq													# qq
-	qqmusic											# qq音乐
-	royal-tsx										# ssh工具客户端
-	teamviewer									# 远程工具
-	tencent-lemon								# 腾讯柠檬清理
-	# tencent-meetin						# 腾讯会议
-	# tunnelblick								# OpenVPN Client 的一个 GUI 版本
-	typora											# markdown 编辑器
-	visual-studio-code					# idea
-	vmware-fusion								# 虚拟机
-	wechat											# 微信
-	# wechatwebdevtools					# 微信开发者工具
-	# zoom											# 远程会议
+	adobe-creative-cloud			# Adobe
+	# aliworkbench				# 千牛卖家工作台
+	battle-net				# 暴雪战网
+	# charles				# 抓包神器 / Mock工具
+	cheatsheet				# 应用快捷键提示
+	clashx-pro				# clashx
+	dash          				# Mac专属的文档管理工具
+	google-chrome				# Google 浏览器
+	iina					# 视频播放器
+	iterm2					# 终端
+	lark					# 飞书
+	neteasemusic				# 网易云
+	# motrix				# 一款全能的下载工具
+	parallels				# 虚拟机
+	# platelet				# 血小板
+	pock					# Touchbar 自定义
+	# postman				# API 请求测试
+	qq					# qq
+	qqmusic					# qq音乐
+	royal-tsx				# ssh工具客户端
+	teamviewer				# 远程工具
+	tencent-lemon				# 腾讯柠檬清理
+	# tencent-meetin			# 腾讯会议
+	# tunnelblick				# OpenVPN Client 的一个 GUI 版本
+	typora					# markdown 编辑器
+	visual-studio-code			# idea
+	vmware-fusion				# 虚拟机
+	wechat					# 微信
+	# wechatwebdevtools			# 微信开发者工具
+	# zoom					# 远程会议
 )
 
 # CLI软件包清单
@@ -63,6 +64,7 @@ brew_cli_app_list=(
 	nvm
 	# yarn
 	# mongodb-community
+	bat
 )
 
 # 安装GUI软件包
