48722cecd79f096671e882e07e855760f2bf8409
Draco <draco.coder@gmail.com>
2022-05-13 14:57:34 +0800

    update oh_my_zsh.sh

--- install_oh_my_zsh.sh
+++ install_oh_my_zsh.sh
@@ -7,11 +7,13 @@ brew install font-sauce-code-pro-nerd-font --cask --cask
 echo -e "安装 oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+cd ～
+
 echo -e "安装 zsh-autosuggestions 自动提示插件"
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 echo -e "安装 zsh-syntax-highlighting 语法高亮插件"
-git clone git://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 # echo "安装 zsh-completions 自动补全插件"
 # git clone https://github.com/zsh-users/zsh-completions.git $ZSH_CUSTOM/plugins/zsh-completions
